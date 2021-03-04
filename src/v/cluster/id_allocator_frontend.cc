30ac2f9831bcb7bf1133f8d0c9ace05183ef1b37
Denis Rystsov <denis@vectorized.io>
2021-03-03 19:06:10 -0800

    adds init/retries to id_allocator_frontend

--- src/v/cluster/id_allocator_frontend.cc
+++ src/v/cluster/id_allocator_frontend.cc
@@ -21,11 +21,12 @@
 #include "config/configuration.h"
 #include "model/namespace.h"
 #include "model/record_batch_reader.h"
-
+#include <seastar/core/coroutine.hh>
 #include <seastar/core/do_with.hh>
 #include <seastar/core/sharded.hh>
 
 namespace cluster {
+using namespace std::chrono_literals;
 
 cluster::errc map_errc_fixme(std::error_code ec);
 
@@ -121,17 +122,24 @@ ss::future<allocate_id_reply>
 id_allocator_frontend::do_allocate_id(model::timeout_clock::duration timeout) {
     auto shard = _shard_table.local().shard_for(model::id_allocator_ntp);
 
-    if (shard == std::nullopt) {
-        vlog(
-          clusterlog.warn,
-          "can't find a shard for {}",
-          model::id_allocator_ntp);
-        return ss::make_ready_future<allocate_id_reply>(
-          allocate_id_reply{0, errc::no_leader_controller});
+    auto i=0;
+    while (!shard && i<10) {
+        co_await ss::sleep(0'100ms);
+        shard = _shard_table.local().shard_for(model::id_allocator_ntp);
     }
 
+    if (!shard) {
+        vlog(clusterlog.warn, "can't find a shard for {}", model::id_allocator_ntp);
+        co_return allocate_id_reply{ 0, errc::no_leader_controller };
+    }
+
+    co_return co_await do_allocate_id(*shard, timeout);
+}
+
+ss::future<allocate_id_reply>
+id_allocator_frontend::do_allocate_id(ss::shard_id shard, model::timeout_clock::duration timeout) {
     return _partition_manager.invoke_on(
-      *shard, _ssg, [timeout](cluster::partition_manager& mgr) mutable {
+      shard, _ssg, [timeout](cluster::partition_manager& mgr) mutable {
           auto partition = mgr.get(model::id_allocator_ntp);
           if (!partition) {
               vlog(
