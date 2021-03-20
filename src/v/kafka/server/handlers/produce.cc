d683d592fb04bed2b05cd8af4aa99307d1bedc1b
Denis Rystsov <denis@vectorized.io>
2021-03-20 00:58:55 -0700

    hooks produce

--- src/v/kafka/server/handlers/produce.cc
+++ src/v/kafka/server/handlers/produce.cc
@@ -9,6 +9,7 @@
 
 #include "kafka/server/handlers/produce.h"
 
+#include "config/configuration.h"
 #include "bytes/iobuf.h"
 #include "cluster/metadata_cache.h"
 #include "cluster/partition_manager.h"
@@ -467,21 +468,21 @@ produce_handler::handle(request_context ctx, ss::smp_service_group ssg) {
      * authorization failed.
      */
     if (request.has_transactional) {
-        // will be removed when all of transactions are implemented
-        return ctx.respond(request.make_error_response(
-          error_code::transactional_id_authorization_failed));
-
-        if (
-          !request.transactional_id
-          || !ctx.authorized(
-            security::acl_operation::write,
-            transactional_id(*request.transactional_id))) {
+        if (!config::shard_local_cfg().enable_transactions.value()) {
             return ctx.respond(request.make_error_response(
               error_code::transactional_id_authorization_failed));
         }
+
+        // if (
+        //   !request.transactional_id
+        //   || !ctx.authorized(
+        //     security::acl_operation::write,
+        //     transactional_id(*request.transactional_id))) {
+        //     return ctx.respond(request.make_error_response(
+        //       error_code::transactional_id_authorization_failed));
+        // }
         // <kafka>Note that authorization to a transactionalId implies
         // ProducerId authorization</kafka>
-
     } else if (request.has_idempotent) {
         if (!ctx.authorized(
               security::acl_operation::idempotent_write,
