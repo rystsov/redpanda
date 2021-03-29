bfb4ab9f57a625ada8a85c171d4ac427bcf9b5b6
Denis Rystsov <denis@vectorized.io>
2021-03-29 11:40:50 -0700

    adding examples

---
+++ examples/tests/functional/src/main/java/io/vectorized/tests/TxRecord.java
@@ -0,0 +1,14 @@
+package io.vectorized.tests;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class TxRecord
+{
+    public long offset;
+    public String key;
+    public String value;
+}
