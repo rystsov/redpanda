fe9b0411320990377a999af10b9cca1f56d111bb
Denis Rystsov <denis@vectorized.io>
2021-04-03 13:14:52 -0700

    fix api

--- src/v/kafka/server/handlers/api_versions.cc
+++ src/v/kafka/server/handlers/api_versions.cc
@@ -54,7 +54,7 @@ using request_types = make_request_types<
   describe_log_dirs_handler,
   create_acls_handler,
   delete_acls_handler,
-  init_producer_id_handler
+  init_producer_id_handler,
   add_partitions_to_txn_handler,
   add_offsets_to_txn_handler,
   txn_offset_commit_handler,
